a56925da54abb7b411f9f1e4b289928aadbd7f46
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-12 23:14:46 +0500

    Add errors

---
+++ shared_models/shared_models/parser/errors.py
@@ -0,0 +1,31 @@
+class InvalidChannelLink(Exception):
+    def __init__(self, url: str, details: str):
+        self.url = url
+        self.details = details
+    
+    def __str__(self):
+        return f"Invalid channel link: {self.url}. Details: {self.details}"
+
+
+class FloodWait(Exception):
+    def __init__(self, seconds: int):
+        self.seconds = seconds
+    
+    def __str__(self):
+        return f"Flood wait: {self.seconds} seconds"
+
+
+class UserBan(Exception):
+    def __init__(self, details: str):
+        self.details = details
+    
+    def __str__(self):
+        return f"User ban: {self.details}"
+
+
+class CannotGetChannelInfo(Exception):
+    def __init__(self, url: str):
+        self.url = url
+    
+    def __str__(self):
+        return f"Cannot get channel info for {self.url}"

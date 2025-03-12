d825ded368011cfd0f85d8bec4b0f111c75b8cc2
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-12 23:15:30 +0500

    Fix telegram init

--- parser/src/telegram/telegram.py
+++ parser/src/telegram/telegram.py
@@ -1,6 +1,7 @@
 import os
 from telethon import TelegramClient
-from ..config import Config
+from tortoise import Tortoise
+from ..config import Config, TORTOISE_ORM
 from .models import Client
 from telegram.opentele.src.td import TDesktop
 from telegram.opentele.src.api import API
@@ -8,40 +9,56 @@ import uuid
 from typing import Optional
 
 class Telegram:
-    def __init__(self, client_id: int) -> None:
-        self.__client_id = client_id
+    def __init__(self) -> None:
         self.__initialized = False
         self.__session_path = os.path.join(Config.SESSION_DIR, f"{uuid.uuid4()}.session")
-        self.__client: Optional[TelegramClient] = None
+        self.__telegram_client: Optional[TelegramClient] = None
+        self.__client: Optional[Client] = None
     
     async def __initialize(self) -> None:
+        await Tortoise.init(config=TORTOISE_ORM)
+        await Tortoise.generate_schemas()
+        
         if self.__initialized:
             return
-        client = await Client.get(id=self.__client_id)
-        if client is None:
-            raise ValueError("Client not found")
-        if client.working == False:
-            raise ValueError("Client is not working")
-        tdata_path = os.path.join(Config.TDATA_PATH, str(client.id), "tdata")
+        
+        self.__client = await Client.filter(working=True).order_by("users_count", "id").first()
+        if self.__client is None:
+            raise ValueError("No active clients found")
+        self.__client.users_count += 1
+        await self.__client.save()
+        
+        tdata_path = os.path.join(Config.TDATA_PATH, str(self.__client.id), "tdata")
         api = API.TelegramDesktop(
-            api_id=client.api_id,
-            api_hash=client.api_hash,
-            device_model=client.device_model,
-            system_version=client.system_version,
-            app_version=client.app_version,
-            lang_code=client.lang_code,
-            system_lang_code=client.system_lang_code,
-            lang_pack=client.lang_pack,
+            api_id=self.__client.api_id,
+            api_hash=self.__client.api_hash,
+            device_model=self.__client.device_model,
+            system_version=self.__client.system_version,
+            app_version=self.__client.app_version,
+            lang_code=self.__client.lang_code,
+            system_lang_code=self.__client.system_lang_code,
+            lang_pack=self.__client.lang_pack,
         )
         tdesk = TDesktop(tdata_path, api)
-        pass_path = os.path.join(Config.TDATA_PATH, str(client.id), "2FA.txt")
+        
+        pass_path = os.path.join(Config.TDATA_PATH, str(self.__client.id), "2FA.txt")
         password = None
         if os.path.exists(pass_path):
             password = open(pass_path).read().strip()
-        self.__client = await tdesk.ToTelethon(self.__session_path, api, password=password) # type: ignore
+            
+        self.__telegram_client = await tdesk.ToTelethon(self.__session_path, api, password=password) # type: ignore
         
     async def get_client(self) -> TelegramClient:
         await self.__initialize()
-        if self.__client is None:
+        if self.__telegram_client is None:
             raise ValueError("Client is None")
-        return self.__client
+        return self.__telegram_client
+    
+    async def close(self) -> None:
+        if self.__telegram_client is not None:
+            await self.__telegram_client.disconnect() # type: ignore
+        self.__telegram_client = None
+        if os.path.exists(self.__session_path):
+            os.remove(self.__session_path)
+        self.__initialized = False
+        await Tortoise.close_connections()

3848d8c6df1d2ce2fb75687e327355203758c1a9
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-16 18:13:16 +0500

    Add routers

--- api/app/config.py
+++ api/app/config.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
 from typing import Any, Dict, Union
-
-
+from arq.connections import RedisSettings
 load_dotenv()
 
 
@@ -12,3 +11,5 @@ class ApiServiceConfig:
                                                               504: {"description": "Timeout Error"},
                                                               400: {"description": "Something went wrong while processing request"},
                                                               200: {"description": "Success"}}
+
+REDIS_SETTINGS = RedisSettings(os.getenv('REDIS_HOST', 'localhost'), int(os.getenv('REDIS_PORT', '6379')))
--- api/app/routers/parser.py
+++ api/app/routers/parser.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, HTTPException
-from app.config import ApiServiceConfig
+from fastapi import APIRouter, HTTPException, File, UploadFile
+from app.config import ApiServiceConfig, REDIS_SETTINGS
+import app.services as services
+from shared_models.parser.get_channel_info import GetChannelInfoResponse, GetChannelInfoRequest
 
 
+parser_service = services.Parser()
 router = APIRouter(prefix="/parser", tags=["Parser"])
 
 
-@router.post("/create_task", responses=ApiServiceConfig.DEFAULT_RESPONSE)
-async def create_task():
-    return {"message": "Task created successfully"}
+@router.post("/add_client", responses=ApiServiceConfig.DEFAULT_RESPONSE)
+async def add_client(tdata: UploadFile = File(...)):
+    await parser_service.add_client(tdata.file.read())
+    return {"message": "Client added successfully"}
+
+@router.post("/get_channel_info", responses=ApiServiceConfig.DEFAULT_RESPONSE, response_model=GetChannelInfoResponse)
+async def get_channel_info(request: GetChannelInfoRequest):
+    return await parser_service.get_channel_info(request)

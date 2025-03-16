b4411d5e44adb27d9086c9fab077fcc2e138af35
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-17 02:32:21 +0500

    Add opprotunity to get 24 hours stat

--- database/src/database.py
+++ database/src/database.py
@@ -6,6 +6,7 @@ from shared_models.database.update_or_create_channel import UpdateOrCreateChanne
 from shared_models.database.get_channel import GetChannelRequest
 from shared_models.database.errors import ChannelDoesNotExistError, StatsDoesNotExistError
 from shared_models.database.get_channels_ids import GetChannelsIdsResponse
+from shared_models.database.get_24h_statistics import Get24hStatisticsRequest, Get24hStatisticsResponse, StatisticsSorting, StatisticsItem
 from .models import Channel, ChannelStatistics
 from tortoise.exceptions import DoesNotExist
 
@@ -72,6 +73,28 @@ class Database:
             views=statistics.views_24h
         )
     
-    async def get_channels_ids(self) -> GetChannelsIdsResponse:
+    @staticmethod
+    async def get_channels_ids(ctx) -> GetChannelsIdsResponse:
         ids = await Channel.all().values_list("id", flat=True)
         return GetChannelsIdsResponse(channel_ids=ids) # type: ignore
+
+    @staticmethod
+    async def get_24h_statistics(ctx, request: Get24hStatisticsRequest) -> Get24hStatisticsResponse:
+        self: Database = ctx['Database_instance']
+        try:
+            channel = await Channel.get(id=request.channel_id)
+        except DoesNotExist:
+            self.logging.error(f"Channel with id {request.channel_id} does not exist")
+            raise ChannelDoesNotExistError(request.channel_id)
+        
+        order_by = "-recorded_at" if request.sorting == StatisticsSorting.NEWEST else "recorded_at"
+        
+        statistics = await ChannelStatistics.filter(channel=channel).order_by(order_by)
+        
+        data = [StatisticsItem(
+                    views=stat.views_24h,
+                    subscribers=stat.subscribers,
+                    time=int(stat.recorded_at.timestamp())
+                ) for stat in statistics]
+        
+        return Get24hStatisticsResponse(sorting=request.sorting, data=data)

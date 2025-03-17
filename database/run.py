71c5dff3ef70fbee3fdb0f68c3988793769d1f76
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-17 14:32:34 +0500

    Add runner

---
+++ database/run.py
@@ -0,0 +1,54 @@
+import logging.config
+from arq import Worker
+from arq.connections import RedisSettings
+import os
+from src import Database
+from arq.logs import default_log_config
+import logging
+from concurrent.futures import ProcessPoolExecutor
+from dotenv import load_dotenv
+import asyncio
+load_dotenv()
+
+
+REDIS_SETTINGS = RedisSettings(os.getenv('REDIS_HOST', 'localhost'), int(os.getenv('REDIS_PORT', '6379')))
+FUNCTIONS = [Database.update_or_create_channel, Database.get_channel, Database.get_channels_ids, Database.get_24h_statistics]
+
+async def startup(ctx):
+    database = Database()
+    await database.connect()
+    ctx['Database_instance'] = database
+    logging.getLogger('arq').info(f'Startup done {ctx["worker_id"]} / {ctx["workers_count"]}')
+    
+
+async def shutdown(ctx):
+    logging.getLogger('arq').info('Shutting down...')
+    await ctx['Telegram_instance'].close()
+
+def start_worker(worker_id: int, workers_count: int):
+    verbose = True
+    log_level = 'DEBUG' if verbose else 'INFO'
+    logging_config = default_log_config(verbose=verbose)
+    logging_config['loggers']['database'] = {'level': log_level, 'handlers': ['arq.standard']}
+    logging_config['loggers']['payment'] = {'level': log_level, 'handlers': ['arq.standard']}
+    
+    logging.config.dictConfig(logging_config)
+    worker = Worker(functions = FUNCTIONS,
+                    on_startup = startup,
+                    on_shutdown = shutdown,
+                    redis_settings = REDIS_SETTINGS,
+                    job_timeout=100,
+                    max_jobs=int(os.getenv('MAX_JOBS', '10')))
+    worker.ctx['worker_id'] = worker_id
+    worker.ctx['workers_count'] = workers_count
+    worker.run()
+
+def main(max_workers: int):
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        for i in range(max_workers):
+            executor.submit(start_worker, i + 1, max_workers)
+
+
+if __name__ == '__main__':
+    main(int(os.getenv('WORKERS_COUNT', '1')))
+    

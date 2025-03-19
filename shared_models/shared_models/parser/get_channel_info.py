2b71e24ce8a00488bd29c23983c2c565e42a79df
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-19 23:35:34 +0500

    Update parser and sheduler models

--- shared_models/shared_models/parser/get_channel_info.py
+++ shared_models/shared_models/parser/get_channel_info.py
@@ -4,7 +4,7 @@ from typing_extensions import Optional
 
 
 class GetChannelInfoRequest(BaseModel):
-    channel_link: Optional[str] = None
+    channel_link: str
     
 class GetChannelInfoResponse(BaseModel):
     channel: Channel

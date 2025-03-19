77d6f773b8cb8e5460b3036a42d0705ef28cdf0f
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-19 23:29:06 +0500

    Add comments ad exceptions raising

--- api/app/routers/parser.py
+++ api/app/routers/parser.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, File, UploadFile
 from app.config import ApiServiceConfig
 import app.services as services
 from shared_models.parser.get_channel_info import GetChannelInfoResponse, GetChannelInfoRequest
+from shared_models.parser.errors import SessionPasswordNeeded, InvalidChannelLink, FloodWait, CannotGetChannelInfo
 
 
 parser_service = services.Parser()
@@ -11,15 +12,25 @@ router = APIRouter(prefix="/parser", tags=["Parser"])
 
 @router.post("/add_client", responses=ApiServiceConfig.DEFAULT_RESPONSE)
 async def add_client(tdata: UploadFile = File(...)):
+    """Add new telegram account"""
     try:
         await telegram_service.add_client(tdata.file.read())
         return {"message": "Client added successfully"}
+    except SessionPasswordNeeded as e:
+        raise HTTPException(status_code=400, detail={"error": "SessionPasswordNeeded", "message": str(e)})
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail={"message": str(e)})
 
 @router.post("/get_channel_info", responses=ApiServiceConfig.DEFAULT_RESPONSE, response_model=GetChannelInfoResponse)
 async def get_channel_info(request: GetChannelInfoRequest):
+    """Get current channel data without writing to the database"""
     try:
         return await parser_service.get_channel_info(request)
+    except InvalidChannelLink as e:
+        raise HTTPException(status_code=400, detail={"error": "InvalidChannelLink", "message": str(e)})
+    except FloodWait as e:
+        raise HTTPException(status_code=400, detail={"error": "FloodWait", "message": str(e)})
+    except CannotGetChannelInfo as e:
+        raise HTTPException(status_code=500, detail={"error": "CannotGetChannelInfo", "message": str(e)})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

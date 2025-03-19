1a01f3fdf6a7237cf87d0ef04d6dd2810598cb4d
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-03-19 23:06:33 +0500

    Change get channel info model

--- shared_models/shared_models/parser/get_channel_info.py
+++ shared_models/shared_models/parser/get_channel_info.py
@@ -1,17 +1,10 @@
-from pydantic import BaseModel, model_validator, ValidationError
+from pydantic import BaseModel
 from ..channel import Channel
-from typing_extensions import Self, Optional
+from typing_extensions import Optional
 
 
 class GetChannelInfoRequest(BaseModel):
     channel_link: Optional[str] = None
-    channel_id: Optional[int] = None
-    
-    @model_validator(mode='after')
-    def check_at_least_one(self) -> Self:
-        if self.channel_link is None and self.channel_id is None:
-            raise ValidationError('channel_link or channel_id must be provided')
-        return self
     
 class GetChannelInfoResponse(BaseModel):
     channel: Channel

d205e2a5bba786d0f2dbb59aeb8996600511dc28
Petr Andreev <dmitrijandreev@MacBook-Pro-Dmitrij.local>
2025-12-03 18:19:20 +0300

    fix

--- parser/src/parser.py
+++ parser/src/parser.py
@@ -195,7 +195,7 @@ class Parser:
                 non_active_client = await self.telegram.get_client()
                 async with non_active_client as client:
                     try:
-                        error = FloodWaitError()
+                        error = FloodWaitError(request=None, message="FLOOD_WAIT")
                         error.seconds = 60
                         raise error
                         return await asyncio.wait_for(
